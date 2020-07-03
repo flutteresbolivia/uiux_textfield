ab543e8487a1f1ee86f465223a65f2e3b460274e
waperp <miguelhangelh@hotmail.com>
2020-07-02 21:10:40 -0400

    First commit

--- lib/text_formfield.dart
+++ lib/text_formfield.dart
@@ -31,23 +31,25 @@ class MyTextFormField extends StatelessWidget {
           hintText: hintText,
           labelText: labelText,
           labelStyle: TextStyle(
-            fontWeight: FontWeight.bold,
-          ),
+              fontWeight: FontWeight.bold,
+              color: Colors.blueAccent,
+              fontSize: 18),
           contentPadding: EdgeInsets.all(20.0),
           enabledBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(width: 2, color: Colors.grey.withOpacity(1)),
+            borderSide:
+                BorderSide(width: 1, color: Colors.blueAccent.withOpacity(1)),
           ),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(width: 2, color: Colors.blue.withOpacity(1)),
+            borderSide: BorderSide(width: 1, color: Colors.blue.withOpacity(1)),
           ),
           focusedBorder: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(width: 2, color: Colors.blue.withOpacity(1)),
+            borderSide: BorderSide(width: 1, color: Colors.blue.withOpacity(1)),
           ),
-          filled: true,
-          fillColor: Colors.white.withOpacity(1),
+          filled: false,
+          fillColor: Colors.blueAccent.withOpacity(0.1),
         ),
         keyboardType: isEmail ? TextInputType.emailAddress : TextInputType.text,
         obscureText: isPassword ? true : false,
